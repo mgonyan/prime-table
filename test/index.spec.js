c8f9d2c1242e3227075f2b016ae7d177bf2fe7a5
Manuel Gonzalez <mgonyan@gmail.com>
2017-01-29 14:58:14 +0000

    Create a prime module in order to return a N number of primes

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,7 +1,37 @@
 'use strict';
 
-const generator = require('prime-generator');
+const expect = require('expect');
+const Prime = require('../index');
 
 describe('Get N prime numbers', () => {
-  // Tests
+  let prime;
+  let isPrimeSpy;
+
+  beforeEach(function () {
+    isPrimeSpy = expect.createSpy();
+    prime = new Prime(isPrimeSpy);
+  });
+
+  afterEach(function () {
+    expect.restoreSpies();
+  });
+
+  it('can created a prime object', () => {
+    expect(new Prime(isPrimeSpy)).toBeA(Prime);
+  });
+  it('should throw an error', () => {
+    expect(() => {new Prime();}).toThrow(/You must specified a prime check function/);
+  });
+  it('should return a empty prime collection when "0" primes has been requested', () => {
+    expect(prime.getPrimes(0)).toEqual([]);
+  });
+  it('should return a empty prime collection when "undefined" primes has been requested', () => {
+    expect(prime.getPrimes()).toEqual([]);
+  });
+  it('should return a collection of 4 prime numbers', () => {
+    isPrimeSpy.andCall((number) => {
+      return [2, 3, 5, 7].includes(number);
+    });
+    expect(prime.getPrimes(4)).toEqual([2, 3, 5, 7]);
+  })
 });
