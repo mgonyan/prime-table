e38a3eb65cc41e5cb1297acd0b35abbf47c20472
Manuel Gonzalez <mgonyan@gmail.com>
2017-01-29 13:37:31 +0000

    First primality check function implement

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 'use strict';
 
+module.exports = (number = 0) => {
+  if (number <= 1) {
+    return false;
+  } else if (number <= 3) {
+    return true;
+  } else if ((number % 2) === 0 || (number % 3) === 0) {
+    return false;
+  }
+
+  let index = 5;
+  while (index * index <= number) {
+    if ((number % index) === 0 || (number % (index + 2)) === 0) {
+      return false;
+    }
+    index += 6;
+  }
+
+  return true;
+};
