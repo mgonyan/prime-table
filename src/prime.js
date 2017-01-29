b3f1a005b017bf2d79bec5194f62bf6374d58e7d
Manuel Gonzalez <mgonyan@gmail.com>
2017-01-29 15:07:30 +0000

    Small refactor and creation of prime module

---
+++ src/prime.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/**
+ * @type {Prime}
+ */
+module.exports = Prime;
+
+/**
+ * Gets the number of prime starting from 2, 3,..., N_(numberOfPrimes),
+ * In order to check if a certain number is prime, needs to receive a
+ * isPrime function which performs the primality test. That flexibility
+ * could allow as to try different algorithms a check its times.
+ *
+ * @param {function} isPrime
+ * @constructor
+ */
+function Prime(isPrime) {
+  if (typeof isPrime !== 'function') {
+    throw new Error('You must specified a prime check function');
+  }
+
+  this.isPrime = isPrime;
+}
+
+/**
+ * Return a collection of consecutive prime numbers
+ *
+ * @param {Number} number, number of prime number we want to get.
+ * @return {Array} primes collection of prime numbers
+ */
+Prime.prototype.getPrimes = function getPrimes(number = 0) {
+  let primes = [];
+
+  let possiblePrime = 2;
+  while(primes.length != number) {
+    if (this.isPrime(possiblePrime)) {
+      primes.push(possiblePrime);
+    }
+
+    ++possiblePrime;
+  }
+
+  return primes;
+};
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,37 +1,8 @@
 'use strict';
 
 const expect = require('expect');
-const Prime = require('../index');
+const primeTable = require('../index');
 
-describe('Get N prime numbers', () => {
-  let prime;
-  let isPrimeSpy;
-
-  beforeEach(function () {
-    isPrimeSpy = expect.createSpy();
-    prime = new Prime(isPrimeSpy);
-  });
-
-  afterEach(function () {
-    expect.restoreSpies();
-  });
-
-  it('can created a prime object', () => {
-    expect(new Prime(isPrimeSpy)).toBeA(Prime);
-  });
-  it('should throw an error', () => {
-    expect(() => {new Prime();}).toThrow(/You must specified a prime check function/);
-  });
-  it('should return a empty prime collection when "0" primes has been requested', () => {
-    expect(prime.getPrimes(0)).toEqual([]);
-  });
-  it('should return a empty prime collection when "undefined" primes has been requested', () => {
-    expect(prime.getPrimes()).toEqual([]);
-  });
-  it('should return a collection of 4 prime numbers', () => {
-    isPrimeSpy.andCall((number) => {
-      return [2, 3, 5, 7].includes(number);
-    });
-    expect(prime.getPrimes(4)).toEqual([2, 3, 5, 7]);
-  })
+describe('Print prime multiplication table', () => {
+  // Tests
 });
