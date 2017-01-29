3778c36276a3025ffa0588246b3df3b4745ae068
Manuel Gonzalez <mgonyan@gmail.com>
2017-01-29 14:09:33 +0000

    Regactor to make the prime check more readable

--- index.js
+++ index.js
@@ -5,17 +5,29 @@ module.exports = (number = 0) => {
     return false;
   } else if (number <= 3) {
     return true;
-  } else if ((number % 2) === 0 || (number % 3) === 0) {
+  } else if (divisibleBy(number, 2) || divisibleBy(number, 3)) {
     return false;
   }
 
   let index = 5;
   while (index * index <= number) {
-    if ((number % index) === 0 || (number % (index + 2)) === 0) {
+    if (divisibleBy(number, index) || divisibleBy(number, index + 2)) {
       return false;
     }
+
     index += 6;
   }
 
   return true;
 };
+
+/**
+ * Check if number/divisor = 0
+ *
+ * @param {number} number
+ * @param {number} divisor
+ * @return {boolean}
+ */
+function divisibleBy(number, divisor) {
+  return (number % divisor === 0);
+}
